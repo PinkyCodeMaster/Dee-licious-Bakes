d98899f544448450bd495baac094ee37734375b5
Scott William Jones <166113361+PinkyCodeMaster@users.noreply.github.com>
2025-07-17 20:15:24 +0100

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  turbopack: {
+    // Example: adding an alias and custom file extension
+    resolveAlias: {
+      underscore: 'lodash',
+    },
+    resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+};
+
